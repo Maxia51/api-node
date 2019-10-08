e00c00ab96624fdeb9f7ca6c045d458b82488872
Maxence Margaine <maxence.margaine@gmail.com>
2019-10-08 21:46:40 +0200

    Implementation get User

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,13 +1,76 @@
 const express = require("express");
 const router = express.Router();
+const http = require("http");
 
 // All User Route
 
 router.get('/', (req, res) => {
 
-    res.writeHead(200);
-    res.end("users");
+    try{
+
+        getUser("http://jsonplaceholder.typicode.com/users", (response) => {
+
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(200);
+
+            res.end(response);
+
+        });
+
+    } catch (err) {
+        res.writeHead(500);
+        res.end();
+    }
 
 });
+
+router.get('/:id', (req, res) => {
+
+    try{
+
+        getUser("http://jsonplaceholder.typicode.com/users/"+req.params.id, (response) => {
+
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(200);
+
+            res.end(response);
+
+        });
+
+    } catch (err) {
+        res.writeHead(500);
+        res.end();
+    }
+
+})
+
+/**
+ * 
+ * @param {string} url url to call
+ * @param {function} callback(response) json response stringified 
+ */
+function getUser(url, callback){
+
+    http.get(url, (response) => {
+
+        let body = '';
+
+        response.on('data', function(chunk){
+            body += chunk;
+        });
+    
+        response.on('end', function(){
+            
+            callback(JSON.stringify(JSON.parse(body)));
+
+        });
+
+        return body;
+
+    }).on('error', (err) => {
+        if (err) throw err;
+    })
+
+}
 
 module.exports = router; 
